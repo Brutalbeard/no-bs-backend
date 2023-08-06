d686e7adb08f0f83c6ed3a5040b506cfe6a967e7
John Celoria <jceloria@gmail.com>
2023-08-06 08:59:33 -0600

    continuing work

--- src/routes/meal-router.ts
+++ src/routes/meal-router.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 /* GET meal listings. */
 router.get('/', async function (req, res, next) {
   let records = await Meal.findAll({
-    limit: 50 | Number(req.query.limit)
+    limit: Number(req.query.limit) ? Number(req.query.limit) : Number(process.env.MEAL_DEFAULT_PAGE_SIZE)
   });
   res.status(200).send(records);
 });
@@ -13,25 +13,33 @@ router.get('/', async function (req, res, next) {
 
 /* GET meal by id. */
 router.get('/:id', async function (req, res, next) {
-  let records = await Meal.findByPk(req.params.id);
-  res.status(200).send(records);
+  await Meal
+    .findByPk(req.params.id)
+    .then((meal) => {
+      if (meal) {
+        res.status(200).send(meal);
+      } else {
+        res.status(404).send('Not found');
+      }
+    });
 });
 
 // a route that posts data to the server
 router.post('/', async function (req, res, next) {
-  let meal = new Meal(req.body)
+  let meal = new Meal(req.body);
 
   await meal.validate()
     .catch((err) => {
-      res.status(400).send(err)
+      res.status(400).send(err);
+      next();
     })
 
   meal.save()
     .then((meal) => {
-      res.status(200).send(meal)
+      res.status(200).send(meal);
     })
     .catch((err) => {
-      res.status(400).send(err)
+      res.status(400).send(err);
     })
 });
 
