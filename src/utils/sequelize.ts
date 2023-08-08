137e4c15821557231fa4dc6bf6ef55204d596481
John Celoria <jceloria@gmail.com>
2023-08-07 18:58:12 -0600

    goodbye code smells

--- src/utils/sequelize.ts
+++ src/utils/sequelize.ts
@@ -2,20 +2,13 @@ import { Sequelize } from "sequelize";
 
 require('dotenv').config();
 
-// const sequelize = new Sequelize({
-//     dialect: 'postgres',
-//     host: process.env.PG_DATABASE_ADDRESS,
-//     port: parseInt(process.env.PG_DATABASE_PORT),
-//     username: process.env.PG_USERNAME,
-//     password: process.env.PG_PASSWORD,
-//     database: process.env.PG_DATABASE_NAME
-// }) 
-
 const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite'
+    dialect: 'postgres',
+    host: process.env.PG_DATABASE_ADDRESS,
+    port: parseInt(process.env.PG_DATABASE_PORT),
+    username: process.env.PG_USERNAME,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE_NAME
 }) 
-
-// const sequelize = new Sequelize('sqlite::memory:');
 
 export default sequelize;
