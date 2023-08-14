96faf43c115b106c7429bbf57f0b482d626891f7
John Celoria <jceloria@gmail.com>
2023-08-14 10:16:02 -0600

    more tests

--- src/routes/daily-plan-router.ts
+++ src/routes/daily-plan-router.ts
@@ -2,28 +2,26 @@ import * as express from 'express';
 import DailyPlan from '../models/daily-plan-model';
 const router = express.Router();
 
-/* GET deep dives listings. */
 router.get('/', function (req, res, next) {
-  res.send('you\'ve reached the deep dive route  ');
+  res
+    .send('you\'ve reached the deep dive route  ');
 });
 
 // a route that posts data to the server
 router.post('/', async function (req, res, next) {
   let dailyPlan = new DailyPlan(req.body);
 
-  dailyPlan.validate()
-    .then(() => {
-      dailyPlan.save()
-        .then((dailyPlan) => {
-          res.status(200).send(dailyPlan)
-        })
-        .catch((err) => {
-          res.status(400).send(err)
-        })
+  dailyPlan.save()
+    .then((dailyPlan) => {
+      res
+        .status(200)
+        .send(dailyPlan)
     })
     .catch((err) => {
-      res.status(400).send(err)
+      res
+        .status(400)
+        .send(err)
     })
 });
 
-export default router;
+export default router; 
