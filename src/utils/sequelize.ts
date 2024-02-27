eab01c91322a41e7a37de0f64b6bc45e2211fd3b
John Celoria <jceloria@gmail.com>
2024-02-26 17:57:46 -0700

    Update sequelize.ts

--- src/utils/sequelize.ts
+++ src/utils/sequelize.ts
@@ -3,22 +3,22 @@ import emitter from "./events";
 
 require('dotenv').config();
 
-// const sequelize = new Sequelize({
-//     dialect: 'sqlite',
-//     storage: './database.sqlite',
-//     logging: false,
-// })
-
 const sequelize = new Sequelize({
-    dialect: 'postgres',
+    dialect: 'sqlite',
     storage: './database.sqlite',
-    host: process.env.PG_DATABASE_ADDRESS,
-    port: parseInt(process.env.PG_DATABASE_PORT),
-    username: process.env.PG_USERNAME,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE_NAME,
-    logging: false
+    logging: false,
 })
+
+// const sequelize = new Sequelize({
+//     dialect: 'postgres',
+//     storage: './database.sqlite',
+//     host: process.env.PG_DATABASE_ADDRESS,
+//     port: parseInt(process.env.PG_DATABASE_PORT),
+//     username: process.env.PG_USERNAME,
+//     password: process.env.PG_PASSWORD,
+//     database: process.env.PG_DATABASE_NAME,
+//     logging: false
+// })
 
 async function setupDatabase() {
 
